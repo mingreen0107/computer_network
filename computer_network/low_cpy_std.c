b0986e92122778f062b46f660f25f8eacdc066c3
minju <71gnos@naver.com>
2023-10-04 15:11:24 +0900

    실습

--- computer_network/low_cpy_std.c
+++ computer_network/low_cpy_std.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <fcntl.h>
-#include <unistd.h>
+//#include <unistd.h>
 #define BUF_SIZE 100
 
 int main(int argc, char* argv[]) {
@@ -9,10 +9,10 @@ int main(int argc, char* argv[]) {
 	char buf[BUF_SIZE];
 
 	/* file open for read-only: O_RDONLY */
-
+	src = open("원본파일.txt", O_RDONLY);
 
 	/* file open for writing: O_CREAT|O_WRONLY|O_TRUNC */
-
+	dst = open("대상파일.txt", O_CREAT | O_WRONLY | O_TRUNC);
 
 	if (src == -1 || dst == -1) {
 		puts("file open error");
@@ -20,6 +20,12 @@ int main(int argc, char* argv[]) {
 	}
 
 	/* read data from the source and write the data to the destination*/
+	while ((read_cnt = read(src, buf, BUF_SIZE)) > 0) {
+		if (write(dst, buf, read_cnt) != read_cnt) {
+			puts("file write error");
+			return -1;
+		}
+	}
 
 	close(src);
 	close(dst);
