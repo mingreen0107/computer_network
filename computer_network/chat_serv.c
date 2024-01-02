eed9894ba22831b8a86dd27eefc23a30effbfa74
minju <71gnos@naver.com>
2024-01-02 10:11:26 +0900

    ass

--- computer_network/chat_serv.c
+++ computer_network/chat_serv.c
@@ -28,6 +28,13 @@ int main(int argc, char* argv[]) {
 		printf("Usage : %s <port>\n", argv[0]);
 		exit(1);
 	}
+	pthread_mutex_init(&mutx, NULL);
+	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_adr.sin_port = htons(atoi(argv[1]));
 
 	if (bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
 		error_handling("bind() error");
