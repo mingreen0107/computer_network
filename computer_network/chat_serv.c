d067972acb6021fed3a4a94961e6c37607e05851
minju <71gnos@naver.com>
2024-02-07 10:14:59 +0900

    ass

--- computer_network/chat_serv.c
+++ computer_network/chat_serv.c
@@ -63,6 +63,7 @@ void* handle_clnt(void* arg) {
 		send_msg(msg, str_len);
 
 	pthread_mutex_lock(&mutx);
+
 	for (i = 0; i < clnt_cnt; i++)   // remove disconnected client
 	{
 		if (clnt_sock == clnt_socks[i])
